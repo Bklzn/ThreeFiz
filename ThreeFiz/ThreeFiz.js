2aceec6e1335022fad8d24fbf8694d0827cb3423
Bklzn <adrian_bon@wp.pl>
2022-11-27 20:28:43 +0100

    box as OBB object

--- ThreeFiz/ThreeFiz.js
+++ ThreeFiz/ThreeFiz.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { Octree } from '../threejs/three/examples/jsm/math/Octree.js';
+import { OBB } from '../threejs/three/examples/jsm/math/OBB.js';
+import { Vector3 } from 'three';
 
 class ThreeFiz{
     constructor(SCENE, world = null,TIME_STEP = .03){
@@ -8,20 +10,21 @@ class ThreeFiz{
         this.accumulator = 0.0
         this.TIME_STEP = TIME_STEP
         this.spheres = []
-        this.cubes = []
+        this.boxes = []
         this.world = world
         this.SCENE = SCENE
         this.GRAVITY = new THREE.Vector3(0,-10,0)
     }
     init(){
-         if(this.world) { // DO POPRAWY
+         if(this.world) { // DO POPRAWY // zamień na PIVOTA i wczytywaj OCTREE doń (możliwość sterowania całymi drzewami ?!?!?!?)
             this.SCENE.add(this.world.mesh)
          }
         this.spheres.forEach((sphere) => {
             this.SCENE.add(sphere.mesh)
         })
-        this.cubes.forEach((cube) => {
-            this.SCENE.add(cube.mesh)
+        this.boxes.forEach((box) => {
+            this.SCENE.add(box.mesh)
+            box.collider = new OBB()
         })
     }
     addSphere(mesh, radius, mass){
@@ -34,6 +37,24 @@ class ThreeFiz{
             hit: false
         }
         this.spheres.push(sphere)
+    }
+    addBox(mesh, mass) { ////////////////////////
+        let box = {
+            mesh: mesh,
+            collider: new OBB(),
+            velocity: new THREE.Vector3(),
+            force: new THREE.Vector3(),
+            mass: mass,
+            hit: false
+        }
+        box.mesh.userData.obb = new OBB()
+        box.mesh.userData.obb.halfSize.copy(new THREE.Vector3(
+            mesh.geometry.parameters.width,
+            mesh.geometry.parameters.height,
+            mesh.geometry.parameters.depth,
+            )).multiplyScalar( 0.5 );
+        box.mesh.matrixAutoUpdate = false;
+        this.boxes.push(box)
     }
     setWorld(mesh){
         let world = {
@@ -50,16 +71,20 @@ class ThreeFiz{
             sphere.collider.center.addScaledVector(sphere.velocity.clone(), time/10000);
             const damping = Math.exp( - 1.1 * time/10 ) - 1;
         })
-        this.cubes.forEach((cube) => {
-            // in future
+        this.boxes.forEach((box) => { //////////////////////////
+            box.mesh.updateMatrix();
+            box.mesh.updateMatrixWorld();
+            box.collider.copy(box.mesh.userData.obb)
+            box.collider.applyMatrix4( box.mesh.matrixWorld ); // WWWWWTTTTFFFFF
         })
+        this.sphereBoxCollisions()
         this.spheresCollisions()
         if (this.world) this.worldCollisions(time) // może do update lepiej
     }
     checkCollisions(obj){
         obj.mesh.position.copy(obj.collider.center)
-        // this.world.collider.subTrees = []
-        // this.world.collider.fromGraphNode(this.world.mesh)
+        this.world.collider.subTrees = []
+        this.world.collider.fromGraphNode(this.world.mesh)
         return this.world.collider.sphereIntersect(obj.collider)
     }
     worldCollisions(time){
@@ -72,7 +97,6 @@ class ThreeFiz{
                 sphere.collider.center.add( collision.normal.clone().multiplyScalar( collision.depth ))
                 sphere.velocity.addScaledVector(collision.normal, -dot * 1.5) // 1.5 to sprężystość, 2 = 100% odbitej energii + damping
                 const damping = Math.exp( - 1.1 * time/500 )
-                // console.log(damping)
                 sphere.velocity.multiplyScalar(damping)
             }
         })
@@ -105,6 +129,37 @@ class ThreeFiz{
                 //     sphere.velocity.addScaledVector(collision.normal, -dot * 1.5) // 1.5 to sprężystość, 2 = 100% odbitej energii + damping
                 // }
             })
+        })
+    }
+    sphereBoxCollisions(){
+        this.boxes.forEach((box) => {
+            this.spheres.forEach((sphere) => {
+                if(box.collider.intersectsSphere(sphere.collider)){
+                    const closestPoint = new THREE.Vector3()
+                    const relativeVelocity = sphere.velocity.clone() //.sub(floor.velocity)
+                    box.collider.clampPoint(sphere.collider.center, closestPoint)
+                    const collision = {
+                        normal: sphere.collider.center.clone().sub(closestPoint).normalize(),
+                        depth: sphere.collider.radius - sphere.collider.center.clone().sub(closestPoint).length(),
+                    }
+                    const dot = relativeVelocity.dot(collision.normal)
+                    sphere.collider.center.add( collision.normal.clone().multiplyScalar( collision.depth ))
+                    sphere.velocity.addScaledVector(collision.normal, -dot * 1.5) // 1.5 to sprężystość, 2 = 100% odbitej energii + damping
+                }
+            })
+        })
+    }
+    boxHelper(){
+    }
+    octreeHelper({ node = this.world.collider, depth = 5, color = 0xFF0000}){
+        let octree = node
+        if(depth == 0){
+            let boxHelper = new THREE.Box3Helper(octree.box, color)
+            this.SCENE.add(boxHelper)
+            return
+        }
+        octree.subTrees.forEach((octreeNode) => {
+            this.octreeHelper({node: octreeNode, depth: depth-1, color: color})
         })
     }
     update(){
--- main.js
+++ main.js
@@ -34,12 +34,14 @@ const load = (path) =>{
         threeFizWorld.setWorld(obj);
         scene.add(obj)
         console.log(obj);
+        console.log(threeFizWorld.world)
+        // threeFizWorld.octreeHelper({})
     },function(error){
         // if(error) console.error(error);
     });
 }
 //objects
-let flGeo= new THREE.BoxGeometry(100,1,100);
+let flGeo= new THREE.BoxGeometry(200,1,100);
 let flMat= new THREE.MeshPhongMaterial({color: 0xFF0000});
 let sphereGeo= new THREE.SphereGeometry(10);
 let sphereMat= new THREE.MeshPhongMaterial({color: 0x0000FF});
@@ -50,24 +52,26 @@ const helplight= new THREE.PointLightHelper(light);
 
 //Scene
 const threeFizWorld = new ThreeFiz(scene)
-let ilosc = 75
+let ilosc = 10
 for(let i = 0; i< ilosc/2; i++){
     threeFizWorld.addSphere(new THREE.Mesh(sphereGeo,sphereMat), 10, 20)
     threeFizWorld.addSphere(new THREE.Mesh(sphereGeo,sphereMat2), 10, 20)
 }
-load('./models/bowl.glb')
+// load('./models/bowl.glb')
 // load('./models/collision-world.glb')
-// threeFizWorld.setWorld(new THREE.Mesh(flGeo,flMat))
+threeFizWorld.addBox(new THREE.Mesh(flGeo,flMat), 10)
 threeFizWorld.init()
+
+// threeFizWorld.boxes[0].mesh.rotation.z = .3
 threeFizWorld.spheres.forEach((sphere, idx) => {
     sphere.collider.center.set(
         Math.random() * 50 - 10,
-        11 * idx,
+        100,
         Math.random() * 50 - 10
     )
     })
 scene.add(
-    // new THREE.Mesh(flGeo,flMat),
+    // new THREE.BoxHelper(threeFizWorld.boxes[0].collider, 0xffff00),
     light,
     helplight,
 );
@@ -83,11 +87,11 @@ document.body.appendChild( stats.domElement );
 
 const gui = new dat.GUI();
 gui.add(threeFizWorld.spheres[0].collider.center,"y",0,500,.01).name('sphere y');
-// gui.add(threeFizWorld.world.mesh.position,"y",-50,50,.001).name('floor y');
+gui.add(threeFizWorld.boxes[0].mesh.position,"y",-50,50,.001).name('floor y');
 // gui.add(threeFizWorld.world.mesh.position,"x",-50,50,.001).name('floor x');
-// gui.add(threeFizWorld.world.mesh.rotation,'z',-Math.PI,Math.PI,.0001).name('floor rotate');
+gui.add(threeFizWorld.boxes[0].mesh.rotation,'z',-Math.PI,Math.PI,.0001).name('floor rotate');
 document.body.appendChild(document.createElement("p"))
-document.body.appendChild(document.createElement("p"))
+// document.body.appendChild(document.createElement("p"))
 
 
 //scenario
@@ -97,7 +101,7 @@ function loopBall(){
         if(sphere.collider.center.y < -1000) {
             sphere.collider.center.set(
                 Math.random() * 50 - 10,
-                11 * idx,
+                11 * idx + 100,
                 Math.random() * 50 - 10
             )
             sphere.velocity.set(0,0,0)
@@ -108,26 +112,26 @@ function loopBall(){
 controls.update();
 const loop = () =>{
     threeFizWorld.update()
-    document.getElementsByTagName("p")[0].innerHTML = `blue: 
-    x${threeFizWorld.spheres[0].collider.center.x.toFixed(2)} 
-    y${threeFizWorld.spheres[0].collider.center.y.toFixed(2)} 
-    z${threeFizWorld.spheres[0].collider.center.z.toFixed(2)}
-    <br>V: (
-        x${threeFizWorld.spheres[0].velocity.x.toFixed(2)} 
-        y${threeFizWorld.spheres[0].velocity.y.toFixed(2)} 
-        z${threeFizWorld.spheres[0].velocity.z.toFixed(2)}
-    )
-    `
-    document.getElementsByTagName("p")[1].innerHTML = `green: 
-    x${threeFizWorld.spheres[1].collider.center.x.toFixed(2)} 
-    y${threeFizWorld.spheres[1].collider.center.y.toFixed(2)} 
-    z${threeFizWorld.spheres[1].collider.center.z.toFixed(2)} 
-    <br>V: (
-        x${threeFizWorld.spheres[1].velocity.x.toFixed(2)} 
-        y${threeFizWorld.spheres[1].velocity.y.toFixed(2)} 
-        z${threeFizWorld.spheres[1].velocity.z.toFixed(2)}
-    )
-    `
+    // document.getElementsByTagName("p")[0].innerHTML = `blue: 
+    // x${threeFizWorld.spheres[0].collider.center.x.toFixed(2)} 
+    // y${threeFizWorld.spheres[0].collider.center.y.toFixed(2)} 
+    // z${threeFizWorld.spheres[0].collider.center.z.toFixed(2)}
+    // <br>V: (
+    //     x${threeFizWorld.spheres[0].velocity.x.toFixed(2)} 
+    //     y${threeFizWorld.spheres[0].velocity.y.toFixed(2)} 
+    //     z${threeFizWorld.spheres[0].velocity.z.toFixed(2)}
+    // )
+    // `
+    // document.getElementsByTagName("p")[1].innerHTML = `green: 
+    // x${threeFizWorld.spheres[1].collider.center.x.toFixed(2)} 
+    // y${threeFizWorld.spheres[1].collider.center.y.toFixed(2)} 
+    // z${threeFizWorld.spheres[1].collider.center.z.toFixed(2)} 
+    // <br>V: (
+    //     x${threeFizWorld.spheres[1].velocity.x.toFixed(2)} 
+    //     y${threeFizWorld.spheres[1].velocity.y.toFixed(2)} 
+    //     z${threeFizWorld.spheres[1].velocity.z.toFixed(2)}
+    // )
+    // `
     controls.update();
     stats.update();
     renderer.render(scene,camera);
