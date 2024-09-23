07a2f2d72019a82b85b335025e11968a8c9dc302
Bklzn <adrian_bon@wp.pl>
2024-09-23 14:04:12 +0200

    fix updating matrixes

--- src/ThreeFiz/World.ts
+++ src/ThreeFiz/World.ts
@@ -23,10 +23,11 @@ class World {
       if (!object.isStatic) {
         this.applyGravity(object, dT);
       }
-      object.mesh.updateMatrix();
-      object.updateAABB();
       object.updatePosition(dT);
       object.updateRotation(dT);
+      object.mesh.updateMatrix();
+      object.mesh.updateMatrixWorld();
+      object.updateAABB();
       object.updateCollider();
       object.debug.update();
     });
