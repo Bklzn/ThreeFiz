1436a44edf57be37f9c95e1dd802930dc648bc21
Bklzn <adrian_bon@wp.pl>
2024-10-03 13:59:46 +0200

    fix

--- src/ThreeFiz/SpatialHash.ts
+++ src/ThreeFiz/SpatialHash.ts
@@ -25,100 +25,102 @@ let line: LineSegments;
 class SpatialHash {
   cellSize: number;
   grid: Map<string, Set<number>>;
-  private hashesPerObject: Map<number, string[]>;
-  private invCellSize: number;
+  private hashesPerObject: Map<number, Set<string>>;
   constructor(cellSize: number) {
     this.cellSize = cellSize;
-    this.invCellSize = 1 / cellSize;
     this.grid = new Map();
     this.hashesPerObject = new Map();
   }
 
-  // Funkcja haszująca dla pozycji
-  private hash(v: Vector3) {
-    const cellX = Math.floor(v.x * this.invCellSize);
-    const cellY = Math.floor(v.y * this.invCellSize);
-    const cellZ = Math.floor(v.z * this.invCellSize);
+  private hash(x: number, y: number, z: number) {
+    const cellX = Math.floor(x / this.cellSize);
+    const cellY = Math.floor(y / this.cellSize);
+    const cellZ = Math.floor(z / this.cellSize);
     return `${cellX},${cellY},${cellZ}`;
   }
 
-  // Dodaj obiekt do siatki
-  create(object: RigidBody, id: number): void {
-    const [mX, mY, mZ] = this.getCell(this.hash(object.aabb.min));
-    const [MX, MY, MZ] = this.getCell(this.hash(object.aabb.max));
+  create(object: RigidBody, id: number) {
+    const [min, max] = this.getBounds(object.aabb);
 
-    const cells: string[] = [];
-    for (let x = mX; x <= MX; x++) {
-      for (let y = mY; y <= MY; y++) {
-        for (let z = mZ; z <= MZ; z++) {
-          const hash = `${x},${y},${z}`;
-          let cell = this.grid.get(hash);
-          if (!cell) {
-            cell = new Set();
-            this.grid.set(hash, cell);
+    const hashes = new Set<string>();
+
+    for (let x = min.x; x <= max.x; x++) {
+      for (let y = min.y; y <= max.y; y++) {
+        for (let z = min.z; z <= max.z; z++) {
+          const key = `${x},${y},${z}`;
+          if (!this.grid.has(key)) {
+            this.grid.set(key, new Set());
           }
-          cell.add(id);
-          cells.push(hash);
+          this.grid.get(key)!.add(id);
+          hashes.add(key);
         }
       }
     }
-    this.hashesPerObject.set(id, cells);
+    this.hashesPerObject.set(id, hashes);
   }
 
   update(object: RigidBody, id: number) {
-    const oldCells = this.hashesPerObject.get(id);
-    const [mX, mY, mZ] = this.getCell(this.hash(object.aabb.min));
-    const [MX, MY, MZ] = this.getCell(this.hash(object.aabb.max));
-
-    const newCells: string[] = [];
-    for (let x = mX; x <= MX; x++) {
-      for (let y = mY; y <= MY; y++) {
-        for (let z = mZ; z <= MZ; z++) {
-          const hash = `${x},${y},${z}`;
-          if (!oldCells || !oldCells.includes(hash)) {
-            let cell = this.grid.get(hash);
-            if (!cell) {
-              cell = new Set();
-              this.grid.set(hash, cell);
-            }
-            cell.add(id);
-          }
-          newCells.push(hash);
+    const oldHashes = this.hashesPerObject.get(id);
+    const [min, max] = this.getBounds(object.aabb);
+    const newHashes = new Set<string>();
+    for (let x = min.x; x <= max.x; x++) {
+      for (let y = min.y; y <= max.y; y++) {
+        for (let z = min.z; z <= max.z; z++) {
+          const key = `${x},${y},${z}`;
+          newHashes.add(key);
         }
       }
     }
     // Remove from old cells that are not in new hashes
-    if (oldCells) {
-      for (const x of oldCells) {
-        if (!newCells.includes(x)) {
-          const cell = this.grid.get(x);
+    this.remove(id, oldHashes, newHashes);
+    // Add to new cells
+    for (const key of newHashes) {
+      if (!oldHashes || !oldHashes.has(key)) {
+        if (!this.grid.has(key)) {
+          this.grid.set(key, new Set());
+        }
+        this.grid.get(key)!.add(id);
+        this.updateVisualizer(key);
+      }
+    }
+    this.hashesPerObject.set(id, newHashes);
+  }
+
+  remove(
+    id: number,
+    oldHashes: Set<string> | undefined,
+    newHashes: Set<string>
+  ) {
+    if (oldHashes) {
+      for (const key of oldHashes) {
+        if (!newHashes.has(key)) {
+          const cell = this.grid.get(key);
           if (cell) {
             cell.delete(id);
             if (cell.size === 0) {
-              this.grid.delete(x);
+              this.grid.delete(key);
+              this.removeVisualizer(key);
             }
           }
         }
       }
     }
-
-    this.hashesPerObject.set(id, newCells);
   }
 
   findNearby(object: RigidBody, id: number) {
-    const [mX, mY, mZ] = this.getCell(this.hash(object.aabb.min));
-    const [MX, MY, MZ] = this.getCell(this.hash(object.aabb.max));
+    const [min, max] = this.getBounds(object.aabb);
 
-    const nearby: number[] = [];
-    for (let x = mX; x <= MX; x++) {
-      for (let y = mY; y <= MY; y++) {
-        for (let z = mZ; z <= MZ; z++) {
-          const hash = `${x},${y},${z}`;
-          const cell = this.grid.get(hash);
+    nearby.clear();
+
+    for (let x = min.x; x <= max.x; x++) {
+      for (let y = min.y; y <= max.y; y++) {
+        for (let z = min.z; z <= max.z; z++) {
+          const key = `${x},${y},${z}`;
+          const cell = this.grid.get(key);
           if (cell) {
-            for (const other of cell) {
+            for (let other of cell) {
               if (other !== id) {
-                nearby.push(other);
+                nearby.add(other);
               }
             }
           }
@@ -130,6 +132,16 @@ class SpatialHash {
   private getCell(hash: string) {
     return hash.split(",").map(Number);
   }
+  private getBounds(box: Box3) {
+    const min = box.min;
+    const max = box.max;
+
+    const [mX, mY, mZ] = this.getCell(this.hash(min.x, min.y, min.z));
+    const [MX, MY, MZ] = this.getCell(this.hash(max.x, max.y, max.z));
+    minV.set(mX, mY, mZ);
+    maxV.set(MX, MY, MZ);
+    return [minV, maxV];
+  }
 
   show(parentScene: Scene) {
     scene = parentScene;
@@ -137,25 +149,23 @@ class SpatialHash {
     e = new EdgesGeometry(g);
     m = new MeshBasicMaterial({ color: 0xff0000 });
     line = new LineSegments(e, m);
-    this.updateVisualizer(this.grid.keys());
+    this.grid.forEach((_, key) => this.updateVisualizer(key));
   }
 
-  private updateVisualizer(keys: Iterable<string>) {
+  private updateVisualizer(key: string) {
     if (!scene) return;
-    for (const key of keys) {
-      if (!GridHelpers.has(key)) {
-        const helper = line.clone();
-        GridHelpers.set(key, helper);
-        scene!.add(helper);
-      }
-      const helper = GridHelpers.get(key)!;
-      const [x, y, z] = this.getCell(key);
-      helper.position.set(
-        x * this.cellSize + this.cellSize / 2,
-        y * this.cellSize + this.cellSize / 2,
-        z * this.cellSize + this.cellSize / 2
-      );
+    if (!GridHelpers.has(key)) {
+      const helper = line.clone();
+      GridHelpers.set(key, helper);
+      scene!.add(helper);
     }
+    const helper = GridHelpers.get(key)!;
+    const [x, y, z] = this.getCell(key);
+    helper.position.set(
+      x * this.cellSize + this.cellSize / 2,
+      y * this.cellSize + this.cellSize / 2,
+      z * this.cellSize + this.cellSize / 2
+    );
   }
 
   private removeVisualizer(key: string) {
