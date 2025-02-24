011f1be2d98755bb84e3545028a8036c36b33c7d
Bklzn <adrian_bon@wp.pl>
2025-02-24 12:17:32 +0100

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/demos",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        twoSpheres: "demos/twoSpheres.html",
+        twoBlocks: "demos/twoBlocks.html",
+        blockSphere: "demos/block&sphere.html",
+        blockFloor: "demos/block&floor.html",
+        objectsFloor: "demos/objects&floor.html",
+        frictionTest: "demos/fricitionTest.html",
+      },
+    },
+  },
 });
